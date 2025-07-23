e871c3d8370abfb7c6bb6cbedca3eb08f07ba2a7
Ariel Jericko Gacilo <bokzgacilo@gmail.com>
2025-07-24 02:58:27 +0800

    kasama logo added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="kasama-logo.png" />
         <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
