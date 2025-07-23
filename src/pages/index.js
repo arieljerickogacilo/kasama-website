6af83fc424b5dd0a4e46e41e175a96a30234bed2
Ariel Jericko Gacilo <bokzgacilo@gmail.com>
2025-07-24 03:00:19 +0800

    add redirect to /career

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/careers"); // use replace to avoid keeping "/" in history
+  }, [router]);
+
   return (
     <>
       <Head>
